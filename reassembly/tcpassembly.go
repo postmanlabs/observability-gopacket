505d1bd07cbc042cf1d1e33abd7774f011235d27
Kevin Ku <kevin@akitasoftware.com>
2020-04-21 10:20:29 -0700

    reassembly: Add AssemblerContext method to ScatterGather interface.
    This method retrieves the AssemblerContext associated with a particular
    offset when processsing reassembled streams, allowing callers to supply
    additional metadata through custom implementations of the
    AssemblerContext interface.
    
    This PR also removes a redundant `CaptureInfo` field within
    `livePacket`.

--- reassembly/tcpassembly.go
+++ reassembly/tcpassembly.go
@@ -98,6 +98,9 @@ type ScatterGather interface {
 	KeepFrom(offset int)
 	// Return CaptureInfo of packet corresponding to given offset
 	CaptureInfo(offset int) gopacket.CaptureInfo
+	// Return AssemblerContext of packet corresponding to given offset. Returns
+	// nil if the offset is invalid.
+	AssemblerContext(offset int) AssemblerContext
 	// Return some info about the reassembled chunks
 	Info() (direction TCPFlowDirection, start bool, end bool, skip int)
 	// Return some stats regarding the state of the stream
@@ -156,19 +159,25 @@ func (rl *reassemblyObject) KeepFrom(offset int) {
 }
 
 func (rl *reassemblyObject) CaptureInfo(offset int) gopacket.CaptureInfo {
+	ac := rl.AssemblerContext(offset)
+	if ac == nil {
+		// Invalid offset
+		return gopacket.CaptureInfo{}
+	}
+	return ac.GetCaptureInfo()
+}
+
+func (rl *reassemblyObject) AssemblerContext(offset int) AssemblerContext {
 	current := 0
 	var r byteContainer
 	for _, r = range rl.all {
-		if current >= offset {
-			return r.captureInfo()
+		if current+r.length() > offset {
+			return r.assemblerContext()
 		}
 		current += r.length()
 	}
-	if r != nil && current >= offset {
-		return r.captureInfo()
-	}
 	// Invalid offset
-	return gopacket.CaptureInfo{}
+	return nil
 }
 
 func (rl *reassemblyObject) Info() (TCPFlowDirection, bool, bool, int) {
@@ -284,7 +293,6 @@ type livePacket struct {
 	bytes []byte
 	start bool
 	end   bool
-	ci    gopacket.CaptureInfo
 	ac    AssemblerContext
 	seq   Sequence
 }
@@ -293,7 +301,7 @@ func (lp *livePacket) getBytes() []byte {
 	return lp.bytes
 }
 func (lp *livePacket) captureInfo() gopacket.CaptureInfo {
-	return lp.ci
+	return lp.ac.GetCaptureInfo()
 }
 func (lp *livePacket) assemblerContext() AssemblerContext {
 	return lp.ac
@@ -317,7 +325,7 @@ func (lp *livePacket) isPacket() bool {
 // Creates a page (or set of pages) from a TCP packet: returns the first and last
 // page in its doubly-linked list of new pages.
 func (lp *livePacket) convertToPages(pc *pageCache, skip int, ac AssemblerContext) (*page, *page, int) {
-	ts := lp.ci.Timestamp
+	ts := lp.captureInfo().Timestamp
 	first := pc.next(ts)
 	current := first
 	current.prev = nil
@@ -722,7 +730,7 @@ func (a *Assembler) AssembleWithContext(netFlow gopacket.Flow, t *layers.TCP, ac
 		}
 	}
 
-	action = a.handleBytes(bytes, seq, half, ci, t.SYN, t.RST || t.FIN, action, ac)
+	action = a.handleBytes(bytes, seq, half, t.SYN, t.RST || t.FIN, action, ac)
 	if len(a.ret) > 0 {
 		action.nextSeq = a.sendToConnection(conn, half, ac)
 	}
@@ -955,12 +963,11 @@ func (a *Assembler) overlapExisting(half *halfconnection, start, end Sequence, b
 }
 
 // Prepare send or queue
-func (a *Assembler) handleBytes(bytes []byte, seq Sequence, half *halfconnection, ci gopacket.CaptureInfo, start bool, end bool, action assemblerAction, ac AssemblerContext) assemblerAction {
+func (a *Assembler) handleBytes(bytes []byte, seq Sequence, half *halfconnection, start bool, end bool, action assemblerAction, ac AssemblerContext) assemblerAction {
 	a.cacheLP.bytes = bytes
 	a.cacheLP.start = start
 	a.cacheLP.end = end
 	a.cacheLP.seq = seq
-	a.cacheLP.ci = ci
 	a.cacheLP.ac = ac
 
 	if action.queue {
